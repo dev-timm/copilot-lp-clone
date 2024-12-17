9b725ab083d805e1c5fa747803f5a517a898ce07
dev-timm <timm.kuemmeth@gmail.com>
2024-12-17 21:56:47 +0100

    set up basic react router functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-const App = () => {
-  return <h1>Hello World</h1>;
-};
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>home</h1>,
+  },
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h2>about page</h2>
+      </div>
+    ),
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 export default App;
